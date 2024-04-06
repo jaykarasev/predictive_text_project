5745c6520ba6d5df7f587ca4c0364f1d927499bd
Jay Karasev <jaykarasev@gmail.com>
2024-04-05 19:29:25 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,84 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+
+function search(str) {
+    return str.length ? fruit.filter(keyword => keyword.toLowerCase().includes(str.toLowerCase())) : [];
+}
+
+function searchHandler() {
+    const inputVal = input.value;
+    const results = search(inputVal);
+    showSuggestions(results); // Call showSuggestions to update the UI with the search results
+}
+
+function showSuggestions(results) {
+    const content = results.map(item => `<li>${item}</li>`).join('');
+    suggestions.innerHTML = content; 
+}
+
+function useSuggestion(e) {
+    if (e.target.tagName === 'LI') { // Ensure the clicked element is an LI
+        input.value = e.target.innerText; // Update the input with the text of the clicked LI
+        suggestions.innerHTML = ''; // Clear the suggestions
+    }
+}
+
+
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion); // Use event delegation for efficiency
+
+
+
+
+// ALTERNATE CODE FOR HIGHLIGHT ON HOVER
+// function highlightSuggestion(e) {
+//     if (e.target.tagName === 'LI') {
+//         e.target.classList.add('highlight'); // Add the highlight class
+//     }
+// }
+
+// function removeHighlight(e) {
+//     if (e.target.tagName === 'LI') {
+//         e.target.classList.remove('highlight'); // Remove the highlight class
+//     }
+// }
+
+// suggestions.addEventListener('mouseover', highlightSuggestion); // Attach event listener for mouseover
+// suggestions.addEventListener('mouseout', removeHighlight); // Attach event listener for mouseout
+
+
+
+
+// ORIGINAL STARTER CODE
+// const input = document.querySelector('#fruit');
+// const suggestions = document.querySelector('.suggestions ul');
+
+// const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+// function search(str) {
+// 	let results = [];
+
+// 	// TODO
+
+// 	return results;
+// }
+
+// function searchHandler(e) {
+// 	// TODO
+// }
+
+// function showSuggestions(results, inputVal) {
+
+// 	// TODO
+// }
+
+// function useSuggestion(e) {
+// 	// TODO
+// }
+
+// input.addEventListener('keyup', searchHandler);
+// suggestions.addEventListener('click', useSuggestion);
